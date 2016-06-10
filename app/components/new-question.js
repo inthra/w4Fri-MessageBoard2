f3ba83155fca8814b7e617e768b29763c4f08580
Inthrayuth Mahaphol <zign.int@gmail.com>
2016-06-10 13:26:15 -0700

    Add new-question component. Create save method for the component.

---
+++ app/components/new-question.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewQuestion: false,
+
+  actions: {
+    questionFormShow() {
+      this.set('addNewQuestion', true);
+    },
+
+    saveQuestion() {
+      var params = {
+        question: this.get('question') ? this.get('question') : "",
+        author: this.get('author') ? this.get('author') : "",
+        note: this.get('note') ? this.get('note') : ""
+      };
+      this.set('addNewQuestion', false);
+      this.sendAction('saveQuestion', params);
+    }
+  }
+});
---
+++ app/routes/question.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model(params) {
+    return this.store.findRecord('question', params.question_id)
+  },
+
+  actions: {
+    saveAnswer(params) {
+      var newAnswer = this.store.createRecord('answer', params);
+      var question = params.topic;
+      question.get('answers').addObject(newAnswer);
+      newAnswer.save().then(function() {
+        return question.save();
+      });
+      this.transitionTo('question', params.topic);
+    },
+  }
+});
