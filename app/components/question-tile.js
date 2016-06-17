367f68f5a260356fe03f3da1f0442c53c8836f16
Inthrayuth Mahaphol <zign.int@gmail.com>
2016-06-17 14:15:11 -0700

    Add style to question and answer on both routes and components. Move update-question component from question.hbs to question-tile.hbs.

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,19 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  isNoteShow: false,
-
   actions: {
-    noteShow() {
-      this.set('isNoteShow', true);
-    },
-    noteHide() {
-      this.set('isNoteShow', false);
-    },
     destroyAnswer(answer) {
       if (confirm('Are you sure you want to delete this answer?')) {
         this.sendAction('destroyAnswer', answer);
       }
+    },
+    destroyQuestion(question) {
+      this.sendAction('destroyQuestion', question);
+    },
+    update(question, params) {
+      this.sendAction('update', question, params);
     }
   }
 });
